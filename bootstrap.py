a1e46539fb2cb8ab72a1c95ff43ab746553b13ea
Peter Tempfli <tempflip@gmail.com>
2012-01-01 21:02:21 +0100

    aa

--- bootstrap.py
+++ bootstrap.py
@@ -20,20 +20,20 @@ def mondat_olvas(nyelv,id):
 cursor.execute("SELECT MAX(id) FROM nyelv1")
 (max_id,)=cursor.fetchall()[0]
 
-def kiegyenlit(mondat1,mondat2):
+def kiegyenlit(mondat1,mondat2): # a ket mondatot egyforma hosszasagura alakitja
 	if len(mondat1) > len(mondat2):
 		while len(mondat1) != len(mondat2): mondat2.append(0)
 	else: 
 		while len(mondat1) != len(mondat2): mondat1.append(0)
 	return(mondat1,mondat2)	
 
-def matrix_lookup(matrix,x,y):
+def matrix_lookup(matrix,x,y): #kikeresi a matrix x,y erteket
 	def_ertek=0.3
 	try: ertek=matrix[x][y]
 	except: ertek=def_ertek
 	return ertek
 
-def matrix_add(matrix,x,y,ertek):
+def matrix_add(matrix,x,y,ertek): # HOZZAAD egy erteket a matrix x,y mezojehez
 	try: matrix[x]	# ha nem leteznek, letrehozzuk a megfelelo mezoket
 	except: 
 		matrix[x]={}
@@ -44,7 +44,7 @@ def matrix_add(matrix,x,y,ertek):
 	matrix[x][y]+=ertek		
 	return matrix
 
-def matrix_mod(matrix,x,y,ertek):
+def matrix_mod(matrix,x,y,ertek): # MEGVALTOZTATJA a matrix x,y erteket
 	try: matrix[x]	# ha nem leteznek, letrehozzuk a megfelelo mezoket
 	except: 
 		matrix[x]={}
@@ -56,13 +56,13 @@ def matrix_mod(matrix,x,y,ertek):
 	return matrix
 
 
-def ertek_szamol(mondat1,mondat2,matrix):
+def ertek_szamol(mondat1,mondat2,matrix): #kiszamolja a HU es EN mondat permutaciojanak erteket a matrix ertekei alapjan
 	ertek=1	
 	for x in range(len(mondat1)):
 		ertek*=matrix_lookup(matrix,mondat1[x],mondat2[x])
 	return ertek
 
-def normalizal(matrix):
+def normalizal(matrix): 	# soronkent normalizalja a matrixot 1-hez
 	for x in matrix.keys():
 		sorszumma=0		
 		for y in matrix[x].keys():
@@ -72,7 +72,9 @@ def normalizal(matrix):
 	return matrix
 
 ###################################
-def perm_csinal(mondat):
+def perm_csinal(mondat): 
+	#permutaciokat csinal ugy, hogy felbontja a mondatot adott hosszusago blokkokra es azokat permutalja kulon
+	#ezaltal keruljuk el, hogy kezelhetetlenul sok permutacio jojjon letre (ennek ara, hogy nem fog szerepelni az osszes)
 	if (len(mondat) > 0):
 		valaszto=6
 		tomb=[]
@@ -102,6 +104,7 @@ def perm_csinal(mondat):
 	else: return []
 
 def perm_csinal_rand(mondat):
+	# itt haromszor csinaljuk meg ugyanazt, mukozben megkeverjuk mondat elemeit, igy nemi valtozatossagot viszunk a dologba
 	mondat2=[x for x in mondat]
 	mondat3=[x for x in mondat]
 	random.shuffle(mondat2)
@@ -116,27 +119,36 @@ def permutacio_csinal(mondat):
 	#return list(itertools.permutations(mondat))
 
 
-	
+# a bootstrap fuggveny csinalja a lenyegi munkat: szo-szo megfeleleseket keres, majd egy ezek valoszinuseget
+# tartalmazo t-matrix-al ter vissza. A kiindulo ertekeket parameterkent kapja, vagyis a tobbszori ujraszamolas alapjan
+# egyre pontosabb adatokat kaphatunk
 def bootstrap(t_matrix):
-	t_matrix_uj={}
-	for i in range(max_id+1)[1:]:
-		print hanyszor,
+	t_matrix_uj={} # ez lesz a visszatero matrix 
+	for i in range(max_id+1)[1:]: # annyiszor fut vegig a ciklus, ahany mondatunk van
+		print hanyszor, #ez azert van, hogy lassuk, eppen hol tart a ciklus
 		print i
+		# beolvassuk a ket mondatot a mondat-adatbazisbol
 		mondat1=mondat_olvas("nyelv1",i)		
 		mondat2=mondat_olvas("nyelv2",i)
+		# a ket mondatot egyforma hosszura igazitjuk, hogy illeszkedjenek egymashoz (ez a permutaciok miatt kell)
 		(mondat1,mondat2)=kiegyenlit(mondat1,mondat2)	
-		if (len(mondat1) > 20): continue ## ha tul hosszu a mondat, ugrunk
+		if (len(mondat1) > 20): continue ## ha tul hosszu a mondat, ugrunk -- nem birna ki a szamitogep
+		#letrehozzuk a masidok nyelvu mondat osszes(valoban nem) letezo elrendezeset (listakat tartalmo lista)
 		permutaciok=permutacio_csinal(mondat2)
+		# ebbe a listaba fog kerulni a minden egyes permutacio altal elert pontszam (ez mutatja meg, mennyire jo az adott elrendezes)
 		ertekek=[]		
-		for j in range(len(permutaciok)):
+		for j in range(len(permutaciok)): # ez a ciklus szamolja ki oket
 			ertekek.append(ertek_szamol(mondat1,permutaciok[j],t_matrix))
 		ertekek=[x / sum(ertekek) for x in ertekek] #normalizaljuk az ertekeket	
+		# ismet vegighaladunk az elrendezeseket
 		for j in range(len(permutaciok)):
-			for k in range(len(permutaciok[j])): #visszairjuk a normalizalt ertekeket az uj matrixba
-				x=mondat1[k]
-				y=permutaciok[j][k]
-				ertek=ertekek[j]
+			# minden egyes szot szemugyre veszunk: az elredendezes altal elert ertekeket HOZZADJUK a t-matrix szo-szo mezojebe
+			for k in range(len(permutaciok[j])):
+				x=mondat1[k]	# az oszlop az elso nyelv mondatanak k-szava
+				y=permutaciok[j][k] # a sor az adott j permutacio k-szava
+				ertek=ertekek[j]	# az ertek, amelyet visszairunk, az ehhez a permutaciohoz tartozo pontszam
 				t_matrix_uj=matrix_add(t_matrix_uj,x,y,ertek)
+	# vegul az egesz  matrixot soronkent normalizaljuk
 	t_matrix_uj=normalizal(t_matrix_uj)
 	return t_matrix_uj
 
@@ -200,15 +212,17 @@ def kimutatas_sqlite (matrix):
 		c.execute("INSERT INTO freq2 (szo, freq) VALUES (?,?)",(x.decode("utf8"),freq_lookup("szotar2",x),))
 	kimutat_db.commit()
 	kimutat_db.close()
-	
+
+# a lenyegi resz itt tortenik: kiszamoljuk a t-matrixot, aztan ujraszamoljuk a kapott ertekekkel,
+# mindezt 'hanyszor' alkalommal
 mtrx={}
 hanyszor=3
 while hanyszor > 0:
 	print "@"
 	mtrx=bootstrap(mtrx)	
 	hanyszor-=1
-	
-#kimutatas(mtrx)
+
+# itt kiirjuk egy sqlite filebe
 kimutatas_sqlite(mtrx)
 connection.close()
 
